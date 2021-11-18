ac1804a42324f523de63ae4ec1f52f9ea19a3d15
jhernandezb <contact@jhernandez.me>
2021-11-18 15:44:35 -0600

    initial fe commit

---
+++ app/tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
